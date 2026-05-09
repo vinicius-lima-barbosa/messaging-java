d3ca09207b519e41b847b1510129f2e5ecc4b768
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2026-05-08 23:09:24 -0300

    feat: adiciona primeira rota do projeto

---
+++ src/main/java/com/messagingjava/MessagingjavaApplication.java
@@ -0,0 +1,13 @@
+package com.messagingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingjavaApplication.class, args);
+	}
+
+}
